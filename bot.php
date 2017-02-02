c15e67f89af1b6ecd78636e7566599b0adb2b2a6
kteerin <kteerin@hotmail.com>
2017-02-02 13:49:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,13 +19,15 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				//'text' => $text
+				'text' => 'find.namo.xyz'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
+				//'messages' => [$messages],
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
